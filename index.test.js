d0f85a8721b898cc242a9fefbe4f67e0feec4cc8
Paul Nodet <paul.nodet@gmail.com>
2021-09-26 18:10:04 +0200

    Update linting config

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import Watchdog from './index.js';
 
-test('Watchdog', (t) => {
+test('Watchdog', t => {
 	const wd = new Watchdog(1000, () => console.log('hello'));
 	wd.start();
 	t.pass();
